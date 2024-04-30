1b65289e10dd0a45a6c7a4cf61e374dcf13e0f26
PrimitiveHCL <83494205+PrimitiveHCL@users.noreply.github.com>
2024-04-30 10:53:59 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    // old "Playground" Database
+    // databaseURL: "https://playground-bfa9f-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+    // "Realtime Database"
+    databaseURL: "https://realtime-database-8eccc-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shopItems")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    // add item into database
+    push(shoppingListInDB, inputValue)
+    console.log(`${inputValue} added to database`)
+
+    // appendToShoppingList(inputValue)
+    clearInputField()
+}
+
+)
+
+onValue(shoppingListInDB, function(snapshot) {
+    
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+
+        clearShoppingList()
+        
+        for (let i=0; i<itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+    
+            appendToShoppingList(currentItem)
+        }
+    } else {
+        shoppingListEl.innerHTML = "No items here... yet"
+    }
+
+})
+
+
+function clearInputField() {
+    inputFieldEl.value = ""
+}
+
+function clearShoppingList() {
+    shoppingListEl.innerHTML = ""
+}
+
+function appendToShoppingList(item) {
+    // shoppingListEl.innerHTML += `<li>${itemValue }</li>`
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+    shoppingListEl.append(newEl)
+
+    newEl.addEventListener("click", function() {
+        let exactLocationOfItemInDB = ref(database,`shopItems/${itemID}`)
+
+        remove(exactLocationOfItemInDB)
+    })
+}
+
